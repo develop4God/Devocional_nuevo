28ec12a1b8ed2154b8bb73762a06151ad9c2811b
develop4God <develop4God@gmail.com>
2025-07-21 18:36:26 -0500

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,17 +15,20 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+// --- ELIMINA O COMENTA ESTAS LÍNEAS ---
+// val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+// rootProject.layout.buildDirectory.value(newBuildDir)
+//
+// subprojects {
+//     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+//     project.layout.buildDirectory.value(newSubprojectBuildDir)
+// }
+// subprojects {
+//     project.evaluationDependsOn(":app")
+// }
+// --------------------------------------
 
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
+// Mantén la tarea clean que es útil para limpiar la carpeta de build
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
