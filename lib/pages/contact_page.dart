7fc7d78849591ab7017e4153f741b82cf03e4412
Develop-Bot-AI <develop.bot.ai@gmail.com>
2025-09-07 21:09:13 -0500

    Fix DropdownButtonFormField initial value assignment

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -169,7 +169,7 @@ class _ContactPageState extends State<ContactPage> {
 
             // Dropdown para seleccionar tipo de contacto
             DropdownButtonFormField<String>(
-              initialValue: _selectedContactOption,
+              value: _selectedContactOption,
               decoration: InputDecoration(
                 labelText: 'contact_page.contact_type_label'.tr(),
                 border: OutlineInputBorder(
