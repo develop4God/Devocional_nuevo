1be36c5bb05197ab798708fc2549bcbd2c50fbec
develop4God <develop4God@gmail.com>
2025-09-08 19:14:23 -0500

    previo a nueva rama para backup con drive

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 
     defaultConfig {
         applicationId = "com.develop4god.devocional_nuevo"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
