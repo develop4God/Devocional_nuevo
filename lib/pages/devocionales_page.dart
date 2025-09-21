a8cea19857da6232fc86e3f5c3530b499c965719
develop4God <develop4God@gmail.com>
2025-09-20 23:29:01 -0500

    cambios en devocionales_page.dart para flag off/on de badges, ya esta habilitado si el parametro esta en off, no se muestra si esta en on se muestra, en firebase remote config

--- lib/pages/devocionales_page.dart
+++ lib/pages/devocionales_page.dart
@@ -16,6 +16,8 @@ import 'package:devocional_nuevo/utils/copyright_utils.dart';
 import 'package:devocional_nuevo/widgets/add_prayer_modal.dart';
 import 'package:devocional_nuevo/widgets/devocionales_page_drawer.dart';
 import 'package:devocional_nuevo/widgets/tts_player_widget.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_tts/flutter_tts.dart';
@@ -50,6 +52,7 @@ class _DevocionalesPageState extends State<DevocionalesPage>
   final FlutterTts _flutterTts = FlutterTts();
 
   AudioController? _audioController;
+  bool _showBadgesTab = false;
 
   @override
   void initState() {
@@ -63,6 +66,7 @@ class _DevocionalesPageState extends State<DevocionalesPage>
     });
 
     _loadInitialData();
+    _loadFeatureFlags();
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
       UpdateService.checkForUpdate();
@@ -158,6 +162,35 @@ class _DevocionalesPageState extends State<DevocionalesPage>
         }
       }
     });
+  }
+
+  Future<void> _loadFeatureFlags() async {
+    try {
+      final remoteConfig = FirebaseRemoteConfig.instance;
+
+      await remoteConfig.setConfigSettings(RemoteConfigSettings(
+        fetchTimeout: const Duration(seconds: 10),
+        minimumFetchInterval: kDebugMode
+            ? const Duration(seconds: 0)
+            : const Duration(minutes: 5),
+      ));
+
+      await remoteConfig.setDefaults({
+        'show_badges_tab': false,
+      });
+
+      await remoteConfig.fetchAndActivate();
+
+      if (mounted) {
+        setState(() {
+          _showBadgesTab = remoteConfig.getBool('show_badges_tab');
+        });
+      }
+
+      developer.log('Badges tab flag loaded: $_showBadgesTab');
+    } catch (e) {
+      developer.log('Failed to load badges flag: $e, keeping default false');
+    }
   }
 
   void _startTrackingCurrentDevocional() {
@@ -968,22 +1001,23 @@ class _DevocionalesPageState extends State<DevocionalesPage>
                         size: 30,
                       ),
                     ),
-                    IconButton(
-                      tooltip: 'donate.my_badges'.tr(),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const MyBadgesPage(),
-                          ),
-                        );
-                      },
-                      icon: Icon(
-                        Icons.military_tech_outlined,
-                        color: appBarForegroundColor,
-                        size: 30,
+                    if (_showBadgesTab)
+                      IconButton(
+                        tooltip: 'donate.my_badges'.tr(),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const MyBadgesPage(),
+                            ),
+                          );
+                        },
+                        icon: Icon(
+                          Icons.military_tech_outlined,
+                          color: appBarForegroundColor,
+                          size: 30,
+                        ),
                       ),
-                    ),
                     IconButton(
                       tooltip: 'tooltips.progress'.tr(),
                       onPressed: () {
