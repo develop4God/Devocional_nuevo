e2b1700a4978a00456b0ddb96481d825f4845ef7
develop4God <develop4God@gmail.com>
2025-10-08 17:23:21 -0500

    Chore: Bump version to 1.1.1+50
    This commit increments the project's build version.
    
    *   **`pubspec.yaml`**: The version has been updated from `1.1.1+49` to `1.1.1+50`.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 
     defaultConfig {
         applicationId = "com.develop4god.devocional_nuevo"
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
